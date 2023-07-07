f5432e0247b15ed25ac4ef63295605406a8c3fa2
camilo <pgalvis253@gmail.com>
2023-07-07 14:54:28 -0500

    adding a exercise

---
+++ utilityhost.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+# Desarollar un programa llamado utilityHost.sh,dentro de el vamos a declarar dos variables una llamada option y otra llamada result e imprima por pantalla el valor
+# Solucion del reto
+
+option=1
+result=5
+
+echo "La Option=$option es  y el Result=$result es"
